ab803ab76f384597d2b82be8303a7834c0088238
Guetta <guetta.xia@outlook.com>
2019-07-09 17:18:13 +0800

    first commit

---
+++ components/articlebrief.vue
@@ -0,0 +1,165 @@
+<template>
+	<view class="articlecard">
+		<view class="title">{{ articlecontent[1] }}</view>
+		<view class="briefarticlecontent">{{ articlecontent[2] }}</view>
+		<view class="picturearea">
+			<image src="../static/0001/pic1.jpg" v-if="articlecontent[3]"></image>
+			<!-- G暂时用地址表示 -->
+			<image src="../static/0001/pic2.jpg" v-if="articlecontent[4]"></image>
+			<image src="../static/0001/pic3.jpg" v-if="articlecontent[5]"></image>
+
+		</view>
+		<view class="tags">
+			<view class="tag" v-for="i in taglist" v-bind:key="i[0]" :style="i[1]">{{i[0]}}</view>
+		</view>
+		<view class="menubar">
+			<image src="../static/touxiang.jpg" class="touxiang"></image>
+			<view class="name">{{ articlecontent[6] }}</view>
+			<view class="time">{{ articlecontent[7] }}</view>
+
+			<view class="icons">
+				<view class="iconbg"></view>
+				<image class="comment" src="../static/icon/comment.svg"></image>
+				<image class="like" src="../static/icon/like.svg"></image>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: 'aticlebrief',
+		props: {
+			articlecontent: {
+				type: Array
+			},
+		},
+		data() {
+			return {
+				taglist: [
+					["123", "background:red"],
+					["13", "background:blue"],
+					["163", "background:yellow"]
+				],
+			};
+		},
+		methods: {},
+		created: function() {
+			console.log('777');
+
+			console.log(this.articlecontent);
+		}
+	};
+</script>
+
+<style>
+	.articlecard {
+		width: 650upx;
+		border-radius: 8px;
+		;
+		margin: 11px auto 0;
+		background-color: #FFFFFF;
+	}
+
+	.title {
+		margin: 16px 25px 0 25px;
+		font-size: 15px;
+		font: MicrosoftYaHei;
+		font-weight: bold;
+		line-height: 19px;
+		margin: 16px 13px 0 15px;
+		padding-top: 16px;
+	}
+
+	.briefarticlecontent {
+		margin: 10px 13px 0 15px;
+		font-size: 13px;
+		line-height: 15px;
+		margin-bottom: 15px;
+
+	}
+
+	.tags {}
+
+	.tag {
+		display: inline-block;
+		border-radius: 10px;
+		padding-left: 5px;
+		padding-right: 5px;
+		margin-left: 5px;
+	}
+
+	.menubar {
+		position: relative;
+		vertical-align: middle;
+		margin-left: 15px;
+		border-radius: 8px;
+	}
+
+	.touxiang {
+		border-radius: 30px;
+		width: 20px;
+		height: 20px;
+		margin-right: 5px;
+		vertical-align: middle;
+	}
+
+	.name {
+		display: inline-block;
+		font-size: 10px;
+		margin-left: 7px;
+		color: #888888;
+	}
+
+	.time {
+		display: inline-block;
+		left: 120px;
+		font-size: 10px;
+		margin-left: 25px;
+		color: #888888;
+	}
+
+	.icons {
+		position: absolute;
+		right: 0;
+		width: 103px;
+		text-align: right;
+		display: inline-block;
+		content-flex: right;
+		overflow: hidden;
+		border-bottom-right-radius: 8px;
+/* 		box-shadow: -4px -2px 2px 0px #888888 inset;
+ */
+	}
+
+	.iconbg {
+		bottom: 0upx;
+		background: #FDD041;
+		position: absolute;
+		width: 100%;
+		height: 50upx;
+		border-top-left-radius: 100upx;
+		/* box-shadow: -2px -2px 2px 1px #808080 inset; */
+		/* G阴影暂时去掉，因为效果不佳 */
+	}
+
+	.icons image {
+		position: relative;
+		/* G添加相对位置 */
+		width: 11px;
+		height: 11px;
+		padding-right: 10px;
+	}
+
+	.picturearea {
+		margin: auto;
+		display: flex;
+		justify-content: center;
+	}
+
+	image {
+		width: 30%;
+		height: 200upx;
+		margin: auto;
+	}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,175 @@
+<template >
+	<view class="index">
+		<mainpagetop></mainpagetop>
+		<!-- <mainpageleft></mainpageleft> -->
+		<articlebrief v-for="(i, index) in showlist" :key="index" v-bind:articlecontent="i"></articlebrief>
+	
+	
+	
+	</view>
+</template>
+
+<script>
+import articlebrief from '../../components/articlebrief'
+import mainpagetop from '../../components/mainpagetop.vue'
+import mainpageleft from '../../components/mainpageleft.vue'
+export default {
+	data() {
+		return {
+			showmenu:'1',
+			title: 'Hello',
+			hottitlelist: ['热门标题111', '热门标题222', '热门标题333'],
+			showlist: [
+				[
+					'0001',
+					'哪位告知一下轮滑广场哪个罗宾汉雕像怎么变成球了',
+					'应该早就不叫轮滑广场了吧。。。。。。也不知道现在叫啥，就是19栋楼下那个，毕业之后那个夏天才把雕像建起来',
+					'',
+					'',
+					'',
+					'陈仅仅',
+					'06-06 16：12'
+				],
+				[
+					'0002',
+					'后街的个人评测',
+					'瘦肉羹 后街靠后的位置 说是瘦肉羹 但看起来就像是面粉吃起来有淡淡的瘦肉的味道 小料自己加 我觉得好吃',
+					'/static/0001/pic1.jpg',
+					'../../static/0001/pic2.jpg',
+					'../../static/0001/pic3.jpg',
+					'陈仅仅',
+					'06-06 16：12'
+				],
+				[
+					'0002',
+					'后街的个人评测',
+					'瘦肉羹 后街靠后的位置 说是瘦肉羹 但看起来就像是面粉吃起来有淡淡的瘦肉的味道 小料自己加 我觉得好吃',
+					'/static/0001/pic1.jpg',
+					'../../static/0001/pic2.jpg',
+					'../../static/0001/pic3.jpg',
+					'陈仅仅',
+					'06-06 16：12'
+				],
+				[
+					'0002',
+					'后街的个人评测',
+					'瘦肉羹 后街靠后的位置 说是瘦肉羹 但看起来就像是面粉吃起来有淡淡的瘦肉的味道 小料自己加 我觉得好吃',
+					'/static/0001/pic1.jpg',
+					'../../static/0001/pic2.jpg',
+					'../../static/0001/pic3.jpg',
+					'陈仅仅',
+					'06-06 16：12'
+				],
+				[
+					'0002',
+					'后街的个人评测',
+					'瘦肉羹 后街靠后的位置 说是瘦肉羹 但看起来就像是面粉吃起来有淡淡的瘦肉的味道 小料自己加 我觉得好吃',
+					'/static/0001/pic1.jpg',
+					'../../static/0001/pic2.jpg',
+					'../../static/0001/pic3.jpg',
+					'陈仅仅',
+					'06-06 16：12'
+				],
+				[
+					'0002',
+					'后街的个人评测',
+					'瘦肉羹 后街靠后的位置 说是瘦肉羹 但看起来就像是面粉吃起来有淡淡的瘦肉的味道 小料自己加 我觉得好吃',
+					'/static/0001/pic1.jpg',
+					'../../static/0001/pic2.jpg',
+					'../../static/0001/pic3.jpg',
+					'陈仅仅',
+					'06-06 16：12'
+				],
+				[
+					'0002',
+					'后街的个人评测',
+					'瘦肉羹 后街靠后的位置 说是瘦肉羹 但看起来就像是面粉吃起来有淡淡的瘦肉的味道 小料自己加 我觉得好吃',
+					'/static/0001/pic1.jpg',
+					'../../static/0001/pic2.jpg',
+					'../../static/0001/pic3.jpg',
+					'陈仅仅',
+					'06-06 16：12'
+				],
+				[
+					'0002',
+					'后街的个人评测',
+					'瘦肉羹 后街靠后的位置 说是瘦肉羹 但看起来就像是面粉吃起来有淡淡的瘦肉的味道 小料自己加 我觉得好吃',
+					'/static/0001/pic1.jpg',
+					'../../static/0001/pic2.jpg',
+					'../../static/0001/pic3.jpg',
+					'陈仅仅',
+					'06-06 16：12'
+				],
+				[
+					'0002',
+					'后街的个人评测',
+					'瘦肉羹 后街靠后的位置 说是瘦肉羹 但看起来就像是面粉吃起来有淡淡的瘦肉的味道 小料自己加 我觉得好吃',
+					'/static/0001/pic1.jpg',
+					'../../static/0001/pic2.jpg',
+					'../../static/0001/pic3.jpg',
+					'陈仅仅',
+					'06-06 16：12'
+				]
+			]
+		};
+	},
+	components: {
+		articlebrief: articlebrief,
+		mainpagetop,
+		mainpageleft,
+	},
+	onLoad() {},
+	methods: {
+		show(){
+			if(this.showmenu==0){
+				this.showmenu=1;
+			}else{this.showmenu=0}
+		}
+	}
+};
+</script>
+
+<style>
+
+.index{	background-color: #F3F3F3;
+		}		
+
+image {
+	width: 60px;
+	height: 60px;
+	display: inline-block;
+}
+.touxiang {
+	border-radius: 30px;
+}
+.search {
+	display: inline-block;
+	width: 70%;
+	box-shadow: inset 0 0 20px #ccc;
+	border-radius: 20upx;
+	margin-left: 50upx;
+}
+.hot {
+	background-color: #e4e4e4;
+	width: 100%;
+}
+.hotitem {
+	margin-bottom: 20px;
+	background: white;
+	height: 27px;
+	border-color: rgb(255, 255, 255);
+	box-shadow: rgb(170, 170, 170) 0px 0px 5px 0px;
+	font-size: 14px;
+	padding: 0px;
+	border-width: 1px;
+	border-style: solid;
+	text-align: left;
+	line-height: 20px;
+	font-weight: normal;
+	font-style: normal;
+}
+.arrow {
+	width: 100upx;
+	height: 60upx;
+}
+</style>
